691424f30d988be79eb5bb8899743c1b406f71c9
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-07-13 17:42:11 +0300

    LABX-295 Migrations: update to set up user recovery address directly to user factory

--- migrations/45_setup_user_factory.js
+++ migrations/45_setup_user_factory.js
@@ -2,11 +2,13 @@
 
 const UserFactory = artifacts.require("UserFactory")
 const UserBackendProvider = artifacts.require("UserBackendProvider")
+const Recovery = artifacts.require("Recovery")
 
 module.exports = (deployer, network, accounts) => {
 	deployer.then(async () => {
 		const userFactory = await UserFactory.deployed()
 		await userFactory.setUserBackendProvider(UserBackendProvider.address)
+		await userFactory.setUserRecoveryAddress(Recovery.address)
 		
 		// NOTE: still need to setup oracle address to be able to create a user with user factory
 		await userFactory.setOracleAddress(accounts[0])
