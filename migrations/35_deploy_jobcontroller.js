c696d05592aa24363d9c67228fe3118a4c2da019
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-09-18 17:28:13 +0300

    LABX-687 Update migrations

--- migrations/35_deploy_jobcontroller.js
+++ migrations/35_deploy_jobcontroller.js
@@ -1,10 +1,17 @@
 "use strict";
 const JobController = artifacts.require('./JobController.sol');
+const JobWorkInitiationControllerLib = artifacts.require("JobWorkInitiationControllerLib")
+const JobWorkProcessControllerLib = artifacts.require("JobWorkProcessControllerLib")
+const JobWorkAcceptanceControllerLib = artifacts.require("JobWorkAcceptanceControllerLib")
 const Roles2Library = artifacts.require('./Roles2Library.sol');
 const Storage = artifacts.require('./Storage.sol');
 
 module.exports = deployer => {
     deployer.then(async () => {
+        await deployer.deploy(JobWorkInitiationControllerLib)
+        await deployer.deploy(JobWorkProcessControllerLib)
+        await deployer.deploy(JobWorkAcceptanceControllerLib)
+
         await deployer.deploy(JobController, Storage.address, 'JobController', Roles2Library.address)
 
         let jobController = await JobController.deployed()
