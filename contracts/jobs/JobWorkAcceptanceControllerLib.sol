429b4fe02ed9877a5d1110ab62c61ca211a6d0e4
Sargon Piraev <sargonpiraev@gmail.com>
2018-10-05 03:57:23 +0300

    LABX-718 contracts

--- contracts/jobs/JobWorkAcceptanceControllerLib.sol
+++ contracts/jobs/JobWorkAcceptanceControllerLib.sol
@@ -60,7 +60,7 @@ contract JobWorkAcceptanceControllerLib is Roles2LibraryAdapter, JobControllerAb
     function acceptWorkResults(uint _jobId)
     external
     onlyClient(_jobId)
-    onlyJobState(_jobId, JOB_STATE_PENDING_FINISH)
+    onlyJobStates(_jobId, JOB_STATE_PENDING_FINISH | JOB_STATE_WORK_REJECTED)
     returns (uint) 
     {
         store.set(jobFinishTime, _jobId, now);
@@ -159,6 +159,157 @@ contract JobWorkAcceptanceControllerLib is Roles2LibraryAdapter, JobControllerAb
 
         _emitter().emitPaymentReleased(_jobId);
         return OK;
+    }
+
+    function acceptWorkResultsAndReleasePayment(uint _jobId)
+    external
+    onlyClient(_jobId)
+    onlyJobStates(_jobId, JOB_STATE_PENDING_FINISH | JOB_STATE_WORK_REJECTED)
+    returns (uint _resultCode)
+    {
+
+        store.set(jobFinishTime, _jobId, now);
+        store.set(jobState, _jobId, JOB_STATE_WORK_ACCEPTED);
+
+        _emitter().emitWorkAccepted(_jobId, now);
+
+        uint payCheck = jobsDataProvider.calculatePaycheck(_jobId);
+        address worker = store.get(jobWorker, _jobId);
+
+        _resultCode = paymentProcessor.releasePayment(
+            bytes32(_jobId),
+            worker,
+            payCheck,
+            store.get(jobClient, _jobId),
+            payCheck,
+            0
+        );
+        if (_resultCode != OK) {
+            return _emitErrorCode(_resultCode);
+        }
+
+        store.set(jobFinalizedAt, _jobId, now);
+        store.set(jobState, _jobId, JOB_STATE_FINALIZED);
+
+        _emitter().emitPaymentReleased(_jobId);
+
+        return OK;
+
+    }
+
+    function delegate(
+        uint _jobId
+    )
+    public
+    onlyClient(_jobId)
+    onlyJobState(_jobId, JOB_STATE_WORK_REJECTED)
+    returns (uint _resultCode)
+    {
+        store.set(jobState, _jobId, JOB_STATE_DELEGATED);
+        _emitter().emitDelegated(_jobId, now);
+    }
+
+    function _sendForRedo(
+        uint _jobId
+    )
+    public
+    returns (uint _resultCode)
+    {
+        store.set(jobState, _jobId, JOB_STATE_STARTED);
+        _emitter().emitSentForRedo(_jobId, now);
+        return OK;
+    }
+
+    function sendForRedoByClient(
+        uint _jobId
+    )
+    public
+    onlyClient(_jobId)
+    onlyJobState(_jobId, JOB_STATE_WORK_REJECTED)
+    returns (uint _resultCode)
+    {
+        return _sendForRedo(_jobId);
+    }
+
+    function sendForRedoByBoardOwner(
+        uint _jobId
+    )
+    public
+    onlyBoardOwner(_jobId)
+    onlyJobState(_jobId, JOB_STATE_DELEGATED)
+    returns (uint _resultCode)
+    {
+        return _sendForRedo(_jobId);
+    }
+
+    function payDelegated(
+        uint _jobId,
+        uint _workerPaycheck,
+        uint _penaltyFee
+    )
+    external
+    onlyJobState(_jobId, JOB_STATE_DELEGATED)
+    onlyBoardOwner(_jobId)
+    returns (uint _resultCode) {
+        return _payDelegated(_jobId, _workerPaycheck, _penaltyFee);
+    }
+
+    function _payDelegated(
+        uint _jobId,
+        uint _workerPaycheck,
+        uint _penaltyFee
+    )
+    private
+    returns (uint _resultCode) {
+
+        uint payCheck = jobsDataProvider.calculateLockAmount(_jobId);
+
+        if (payCheck < _workerPaycheck) {
+            return _emitErrorCode(JOB_CONTROLLER_INVALID_WORKER_PAYCHECK_VALUE);
+        }
+
+        address worker = store.get(jobWorker, _jobId);
+        address client = store.get(jobClient, _jobId);
+
+        if (_workerPaycheck > 0) {
+
+            _resultCode = paymentProcessor.releasePayment(
+                bytes32(_jobId),
+                worker,
+                _workerPaycheck,
+                client,
+                0,
+                _penaltyFee
+            );
+
+            if (_resultCode != OK) {
+                return _emitErrorCode(_resultCode);
+            }
+
+        } else {
+
+            _resultCode = paymentProcessor.releasePayment(
+                bytes32(_jobId),
+                client,
+                payCheck,
+                client,
+                0,
+                _penaltyFee
+            );
+
+            if (_resultCode != OK) {
+                return _emitErrorCode(_resultCode);
+            }
+
+        }
+
+        store.set(jobFinalizedAt, _jobId, _getJobState(_jobId));
+        store.set(jobState, _jobId, JOB_STATE_FINALIZED);
+
+        _emitter().emitPaidDelegated(_jobId, now);
+
+        return OK;
+
     }
 
     function _getJobState(uint _jobId) private view returns (uint) {
